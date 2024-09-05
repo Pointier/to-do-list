ef8d609614991e9879bb3d0ad9a6ff4875adc283
Hugo <hugo.pointier@gmail.com>
2024-09-06 01:26:07 +0200

    added the display function of a todo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import "./style.css";
 import { ToDo, Priority, ChecklistItem } from "./module/to-do";
 const testDate = new Date("December 17, 1995 03:24:00");
 const checklist: ChecklistItem[] = [
   { description: "voili voilu", completed: false },
 ];
+const checklist2: ChecklistItem[] = [];
 const toDo = new ToDo(
   "test",
   "my first to do",
@@ -14,3 +16,42 @@ const toDo = new ToDo(
 const testDiv = document.createElement("div");
 testDiv.innerText = toDo.description;
 document.body.appendChild(testDiv);
+
+function displayToDo(list: ToDo, appendTo: Element) {
+  const listElem = document.createElement("div");
+  listElem.classList.add("toDoElem");
+
+  const title = document.createElement("div");
+  title.innerText = "title : " + list.title;
+  listElem.appendChild(title);
+
+  const description = document.createElement("div");
+  description.innerText = "description : " + list.description;
+  listElem.appendChild(description);
+
+  const date = document.createElement("div");
+  date.innerText = "date : " + list.dueDate;
+  listElem.appendChild(date);
+
+  const priority = document.createElement("div");
+  priority.innerText = "priority : " + list.priority;
+  listElem.appendChild(priority);
+
+  const notes = document.createElement("div");
+  notes.innerText = "notes : " + list.notes;
+  listElem.appendChild(notes);
+
+  for (const elem of list.checklist) {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = elem.completed;
+    listElem.appendChild(checkbox);
+    const checkDescription = document.createElement("span");
+    checkDescription.innerText = elem.description;
+    listElem.appendChild(checkDescription);
+  }
+
+  appendTo.appendChild(listElem);
+}
+
+displayToDo(toDo, document.body);
